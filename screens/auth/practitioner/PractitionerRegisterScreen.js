f15ef1d8124b77b86bd8b8e0fc0434564f5ce386
Mir-Danish <mirdilber43@gmail.com>
2025-10-03 15:51:40 +0530

    Added code to Screens

---
+++ screens/auth/practitioner/PractitionerRegisterScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PractitionerRegisterScreen = () => {
+  return (
+    <View>
+      <Text>PractitionerRegisterScreen</Text>
+    </View>
+  )
+}
+
+export default PractitionerRegisterScreen
+
+const styles = StyleSheet.create({})
